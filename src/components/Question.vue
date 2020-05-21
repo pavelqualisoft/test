b239ceb4cf708cf2e6d0043e53419d3e78d34b33
Pavel Qualisoft <pavelqualisoft@gmail.com>
2020-05-21 17:04:33 +0300

    start functionality

---
+++ src/components/Question.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="question">{{ question.title }}</div>
+    <hr>
+    <div class="ansvers">
+      <label v-for="(ansver, index) in question.ansvers" :key="index">
+        <!-- <input :type="question.type" :name="ansver.name" :value="ansver.value" @change="checkInput($event)"> -->
+        <input :type="question.type" :name="ansver.name" :value="ansver.value" v-model="vars[ansver.name]">
+        {{ ansver.text }} - {{ vars }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: Object
+  },
+  data() {
+    return {
+      vars: {}
+    }
+  },
+  methods: {
+    checkInput(event) {
+      this.$emit('check-input', event.target);
+    },
+  },
+}
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="test">
+    <h2>Test</h2>
+    <hr>
+    <question :question="questions[position]"
+      @check-input="checkingInput"
+    ></question>
+    <hr>
+    <button class="btn btn__next">Далее</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Question from "@/components/Question.vue";
+
+export default {
+  name: "Test",
+  components: {
+    Question
+  },
+  data() {
+    return {
+      userAnswers: [],
+      position: 0,
+      questions: [
+        {
+          type: 'radio',
+          title: 'Какой тег задаёт ссылку?',
+          ansvers: [
+            {
+              name: 'teg',
+              value: 'a',
+              text: 'a'
+            },
+            {
+              name: 'teg',
+              value: 'div',
+              text: 'div'
+            },
+            {
+              name: 'teg',
+              value: 'span',
+              text: 'span'
+            },
+            {
+              name: 'teg',
+              value: 'img',
+              text: 'img'
+            },
+          ]
+        },
+      ]
+    };
+  },
+  methods: {
+    checkingInput(target) {
+      var aData = {};
+      aData[target.value] = target.checked;
+      // this.userAnswers.splice(this.position, 1, aData);
+      // if (typeof this.userAnswers[this.position] == 'undefined') {
+        this.userAnswers[this.position] = aData;
+      // }
+      // this.userAnswers.splice(this.position, 1, aData);
+      console.log(this.userAnswers);
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.test {
+  text-align: left;
+  max-width: 1280px;
+  margin: 0 auto;
+
+  h2 {
+    text-align: center;
+    text-transform: uppercase;
+  }
+
+  label {
+    display: block;
+  }
+}
+</style>
